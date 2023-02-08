02715106c19b9a9c86e06638d11fa0d362a41b39
RainbowHell <awper9794@gmail.com>
2023-02-08 16:48:59 +1000

    Add comments

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
+// Google Sheet的ID藏在網址裡面
+// Google sheet的連結: https://docs.google.com/spreadsheets/d/1hb2XiLriTd_RzLYDDxsQwSmflKVDJCdSnY-5VNqYR68/edit#gid=0
 const SHEET_ID = '1hb2XiLriTd_RzLYDDxsQwSmflKVDJCdSnY-5VNqYR68';
-const REFRESH_TOKEN = '1//04HL6a9oDLk66CgYIARAAGAQSNwF-L9IraNYLBrypFllx2E94rIh1VUE0EjVD_J0Cj0G2gBnmthEqwyOFdjo4A2DNr1jhOaHjxqI';
-const ACCESS_TOKEN = 'ya29.a0AVvZVspKK9sc3TY3EnJmc0_BTjVYdyVVJw73x3tJlwozuPxuCfcfcRv3qGM6ADbOt4OxVY2wmpiwRI895aVQETcURcX0GwfgiirTge7hNLhuW-lQjbGlKdM8iolcWc-JRScyluUsqufrYavaUnRGjBmFDqNoaCgYKAQ4SARMSFQGbdwaIn8LinHz-4lS1ULe4cnv2pw0163';
-const RANGE = 'A1:B5';
-const PAGE1_ID = '0';
-const PAGE2_ID = '185204935';
+
+// 存取Google Sheet API v4的API key
+// 如果題庫讀取失敗，顯示錯誤為憑證錯誤的話，打開一個任意的Google Cloud Project新建一個API key即可
+const API_KEY = 'AIzaSyAb8fdJjnhKwj0LlZWz3kcC0Uz7kxyIQXc';
+
+//工作表(Sheet)的名稱
+const PAGE1_NAME = '1 - iPhone';
 const PAGE2_NAME = '2 - Samsung';
+const PAGE3_NAME = '3 - iPad';
+
+//提取資料的範圍
+const PAGE1_RANGE = 'A2:C10';
 
 getSheetValues = async () =>{
-    const request = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${PAGE2_NAME}!${RANGE}`,
+    const request = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${PAGE1_NAME}!${PAGE1_RANGE}?key=${API_KEY}`,
     {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${ACCESS_TOKEN}`  
-    }
+        headers: 
+        {
+        "Content-Type": "application/json" 
+        }
     });
     const data = await request.json();
     console.log(data);
